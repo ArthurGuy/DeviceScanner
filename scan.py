dfb0ea47356137f0360a0ced75c8b5cb43dec85c
Arthur Guy <arthur@assys.net>
2015-06-15 00:38:19 +0100

    Update scan.py

--- scan.py
+++ scan.py
@@ -2,7 +2,7 @@ import bluetooth
 
 print("performing inquiry...")
 
-nearby_devices = bluetooth.discover_devices(duration=8, lookup_names=True, flush_cache=True)
+nearby_devices = bluetooth.discover_devices(lookup_names=True, flush_cache=True)
 
 print("found %d devices" % len(nearby_devices))
 
