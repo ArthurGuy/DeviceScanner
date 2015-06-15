40bb6d3ec723be917d01dd4840522d9bb64389e4
Arthur Guy <arthur@assys.net>
2015-06-15 05:52:11 +0100

    Update scan.py

--- scan.py
+++ scan.py
@@ -15,6 +15,8 @@ proc = Popen(["sudo", "hcitool", "lescan"], stdout=PIPE, bufsize=1) # start proc
 
 time.sleep(20)
 
+proc.kill()
+
 for line in iter(proc.stdout.readline, b''): # read output line-by-line
     print line,
 # reached EOF, nothing more to read
