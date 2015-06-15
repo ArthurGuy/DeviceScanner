d8634c114225aee89232b9dd80afa13d44ed0c0d
Arthur Guy <arthur@assys.net>
2015-06-15 05:37:28 +0100

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,4 +1,6 @@
-from subprocess import Popen, PIPE, STDOUT
+import collections
+import subprocess
+import threading
 import os
 
 
@@ -9,12 +11,19 @@ os.system("sudo hciconfig hci0 up")
 
 print("performing scan...")
 
-proc = Popen(["sudo timeout 20 hcitool lescan"], stdout=PIPE, shell=True)
-(device, err) = proc.communicate()
+#proc = Popen(["sudo timeout 20 hcitool lescan"], stdout=PIPE, shell=True)
+# start process, redirect stdout
+process = subprocess.Popen(["sudo hcitool lescan"], stdout=subprocess.PIPE, close_fds=True)
 
-#Print bluetooth devices
-print device
+# terminate process in timeout seconds
+timeout = 20 # seconds
+timer = threading.Timer(timeout, process.terminate)
+timer.start()
 
-print err;
+# save last `number_of_lines` lines of the process output
+number_of_lines = 20
+q = collections.deque(process.stdout, maxlen=number_of_lines)
+timer.cancel()
 
-print "Exit status", proc.returncode
+# print saved lines
+print ''.join(q),
