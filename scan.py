fc88b2f2d0bfca9f8e60e4498cf5fb000fad05a6
Arthur Guy <arthur@assys.net>
2015-06-15 05:21:38 +0100

    Update scan.py

--- scan.py
+++ scan.py
@@ -2,7 +2,7 @@ from subprocess import Popen, PIPE, STDOUT
 
 print("performing scan...")
 
-proc = Popen(["sudo", "timeout", "20", "hcitool", "lescan"], stdout=PIPE, stderr=STDOUT, shell=True)
+proc = Popen(["sudo timeout 20 hcitool lescan"], stdout=PIPE, stderr=STDOUT, shell=True)
 (device, err) = proc.communicate()
 
 #Print bluetooth devices
