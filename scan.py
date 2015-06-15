bd30bd445f3391dba526125fff9469bd942e66a7
Arthur Guy <arthur@assys.net>
2015-06-15 05:13:53 +0100

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,10 +1,13 @@
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, STDOUT
 
 print("performing scan...")
 
-proc = Popen(["sudo", "hcitool", "lescan"], stdout=PIPE, bufsize=1) # start process
+proc = Popen(["sudo", "hcitool", "lescan"], stdout=PIPE, bufsize=1, stderr=STDOUT, close_fds=True) # start process
 for line in iter(proc.stdout.readline, b''): # read output line-by-line
     print line,
-# reached EOF, nothing more to read
-proc.communicate() # close `proc.stdout`, wait for child process to terminate
+
+proc.stdout.close()
+proc.wait()
+
+#proc.communicate() # close `proc.stdout`, wait for child process to terminate
 print "Exit status", proc.returncode
