c1978ccca659b3eb3ddfb42a7d8a07b7c41e362c
Arthur Guy <arthur@assys.net>
2015-06-15 05:44:42 +0100

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,29 +1,30 @@
-import collections
 import subprocess
-import threading
+import tempfile
+import time
 import os
-
 
 #Reset Bluetooth interface, hci0
 os.system("sudo hciconfig hci0 down")
 os.system("sudo hciconfig hci0 up")
 
-
 print("performing scan...")
 
-#proc = Popen(["sudo timeout 20 hcitool lescan"], stdout=PIPE, shell=True)
-# start process, redirect stdout
-process = subprocess.Popen(["sudo hcitool lescan"], stdout=subprocess.PIPE)
+# open temporary file (it automatically deleted when it is closed)
+    #  `Popen` requires `f.fileno()` so `SpooledTemporaryFile` adds nothing here
+    f = tempfile.TemporaryFile() 
 
-# terminate process in timeout seconds
-timeout = 20 # seconds
-timer = threading.Timer(timeout, process.terminate)
-timer.start()
+    # start process, redirect stdout
+    p = subprocess.Popen(["sudo hcitool lescan"], stdout=f)
 
-# save last `number_of_lines` lines of the process output
-number_of_lines = 20
-q = collections.deque(process.stdout, maxlen=number_of_lines)
-timer.cancel()
+    # wait 2 seconds
+    time.sleep(20)
 
-# print saved lines
-print ''.join(q)
+    # kill process
+    #NOTE: if it doesn't kill the process then `p.wait()` blocks forever
+    p.terminate() 
+    p.wait() # wait for the process to terminate otherwise the output is garbled
+
+    # print saved output
+    f.seek(0) # rewind to the beginning of the file
+    print f.read(), 
+    f.close()
