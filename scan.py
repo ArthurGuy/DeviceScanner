83bfe2b03df651cbf2dab6f6133cc8d621a8948b
Arthur Guy <arthur@assys.net>
2015-06-15 06:31:06 +0100

    Update scan.py

--- scan.py
+++ scan.py
@@ -3,22 +3,27 @@ import tempfile
 import time
 import os
 
-#Reset Bluetooth interface, hci0
+print("performing scan...")
+
+#Reset Bluetooth interface
 os.system("sudo hciconfig hci0 down")
 os.system("sudo hciconfig hci0 up")
-
-print("performing scan...")
 
 #Start the scan
 os.system("sudo hcitool lescan > results.txt &")
 
+#wait for the results
 time.sleep(10)
 
 #Stop the scan
 os.system("sudo pkill --signal SIGINT hcitool")
 
-#Output the results
-with open('results.txt', 'r') as f:
-    read_data = f.read()
-    print read_data
+#Move the results file into a list
+f = open('results.txt', 'r')
+results = f.readlines()
 f.closed
+
+#Remove empty lines from the list
+str_list = filter(None, str_list)
+
+print results
