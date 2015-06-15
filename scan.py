f77b44fa6ace75213b41270b25ba517c49c0a9e6
Arthur Guy <arthur@assys.net>
2015-06-15 05:27:58 +0100

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,5 +1,11 @@
 from subprocess import Popen, PIPE, STDOUT
 
+
+#Reset Bluetooth interface, hci0
+os.system("sudo hciconfig hci0 down")
+os.system("sudo hciconfig hci0 up")
+
+
 print("performing scan...")
 
 proc = Popen(["sudo timeout 20 hcitool lescan"], stdout=PIPE, stderr=STDOUT, shell=True)
