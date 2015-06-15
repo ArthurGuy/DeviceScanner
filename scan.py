214b0ddfcc8908955b72f5ee42d8556068d900a7
Arthur Guy <arthur@assys.net>
2015-06-15 04:33:43 +0100

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,13 +1,35 @@
-import bluetooth
 
 print("performing inquiry...")
 
-nearby_devices = bluetooth.discover_devices(lookup_names=True, flush_cache=True)
-
-print("found %d devices" % len(nearby_devices))
-
-for addr, name in nearby_devices:
-    try:
-        print("  %s - %s" % (addr, name))
-    except UnicodeEncodeError:
-        print("  %s - %s" % (addr, name.encode('utf-8', 'replace')))
+from os import kill
+import signal
+import subprocess
+import threading
+import tempfile
+import sys
+import time
+from tempfile import TemporaryFile
+import commands
+t = TemporaryFile()
+global pipe_output
+print commands.getoutput("hcitool dev")
+print 'down'
+commands.getoutput('hciconfig hci0 down')
+print 'up'
+commands.getoutput('hciconfig hci0 up')
+print commands.getoutput("hcitool dev")
+commands.getoutput('killall hcitool')
+p = subprocess.Popen('hcitool lescan', bufsize = 0,shell = True, stdout =subprocess.PIPE,stderr = subprocess.STDOUT)
+time.sleep(10)
+#os.kill(p.pid,signal.SIGTERM)
+for i in range(0,30,1):
+    print 'for'
+    inchar = p.stdout.readline()
+    i+=1
+    if inchar:
+        print 'loop num:',i
+        print str(inchar)
+        t.write(str(inchar))
+print 'out of loop'
+t.seek(0)
+print t.read()
