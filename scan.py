c56a793f70ddd422c3298beda4f25c8260a3f47f
Arthur Guy <arthur@assys.net>
2015-06-15 05:18:44 +0100

    Update scan.py

--- scan.py
+++ scan.py
@@ -2,12 +2,11 @@ from subprocess import Popen, PIPE, STDOUT
 
 print("performing scan...")
 
-proc = Popen(["sudo", "hcitool", "lescan"], stdout=PIPE, bufsize=1, stderr=STDOUT, close_fds=True) # start process
-for line in iter(proc.stdout.readline, b''): # read output line-by-line
-    print line,
+proc = Popen(["sudo", "timeout", "20", "hcitool", "lescan"], stdout=PIPE, bufsize=1, stderr=STDOUT, close_fds=True) # start process
+(device, err) = proc.communicate()
 
-proc.stdout.close()
-proc.wait()
+#Print bluetooth devices
+print device
 
-#proc.communicate() # close `proc.stdout`, wait for child process to terminate
+
 print "Exit status", proc.returncode
